f03153e6d210d8e3e087a1d2ea90741fc90536b3
wdku5791 <wdku5791@gmail.com>
2017-03-06 20:20:27 -0800

    commited come will pass the cookieparser tests truely.

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,5 +1,14 @@
 var parseCookies = function(req, res, next) {
-  console.log('-------------', req.headers, '--------------');
+  
+  // console.log(req.headers.hasOwnProperty('cookie'));
+    // console.log(cookies);
+  if (req.headers.hasOwnProperty('cookie')) {
+    var cookies = (req.headers.cookie).split('; ');
+    cookies.forEach(function(item) {
+      var tuple = item.split('=');
+      req.cookies[tuple[0]] = tuple[1];
+    });
+  }
 };
 
 module.exports = parseCookies;
